49a997e211f347cbce052b0ac47d984a6b2ef806
Ciastelix <tenlegitmati@gmail.com>
2023-07-20 17:23:45 +0200

    added CORS

--- users/main.py
+++ users/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 import endpoints
 from containers import Container
+from fastapi.middleware.cors import CORSMiddleware
 
 container = Container()
 
 db = container.db()
 db.create_database()
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.container = container
 app.include_router(endpoints.router)
 if __name__ == "__main__":
